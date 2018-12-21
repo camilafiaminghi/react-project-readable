202755e423c43c181575c5029df23c588f97184e
camila_fiaminghi <camila.fiaminghi@gmail.com>
2018-12-21 15:02:39 -0200

    test NewPost component

---
+++ frontend/src/containers/NewPost.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router'
+import configureMockStore from 'redux-mock-store'
+import { NewPost } from './NewPost'
+import ButtonGoBack from '../components/ButtonGoBack'
+import data from '../__helpers__/categories'
+
+const initialState = {
+	form : {
+		author: '',
+		title: '',
+		body: '',
+		category: 'none'
+	},
+	validation: {
+		author: false,
+		title: false,
+		category: false
+	},
+	validated: false,
+	submitted: false
+}
+const mockStore = configureMockStore([thunk])
+let store
+let wrapper
+const goBack = jest.fn()
+const handleSubmit = jest.fn();
+
+describe('<NewPost />', () => {
+	beforeEach(() => {
+
+		store = mockStore({})
+
+		const provider = mount(
+			<Provider store={store}>
+				<NewPost history={{goBack}} categories={data.categories} />
+			</Provider>
+		)
+
+		wrapper = provider.find(NewPost)
+	})
+
+	it('should render', () => {
+		expect(wrapper).toBeTruthy();
+	})
+
+	it('should renders default elements', () => {
+		expect(wrapper.find(ButtonGoBack).find('button').type()).toBe('button')
+		expect(wrapper.find('h2').exists).toBeTruthy()
+		expect(wrapper.find('form').exists).toBeTruthy()
+		expect(wrapper.find('input[name="author"]').exists).toBeTruthy()
+		expect(wrapper.find('input[name="category"]').exists).toBeTruthy()
+		expect(wrapper.find('input[name="title"]').exists).toBeTruthy()
+		expect(wrapper.find('input[name="body"]').exists).toBeTruthy()
+		expect(wrapper.find('button[type="submit"]').exists).toBeTruthy()
+	})
+
+	it('should have initial state', () => {
+		expect(wrapper.state()).toMatchObject(initialState)
+	})
+
+	it('should submit the form', () => {
+		const form = wrapper.find('form')
+		const button = wrapper.find('button[type="submit"]')
+
+		wrapper.instance().handleSubmit = handleSubmit
+		button.simulate('submit', { preventDefault () {} })
+		expect(wrapper.state().submitted).toEqual(true)
+	})
+})
+
