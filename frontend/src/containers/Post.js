37efb6d0a9cc79080984fc72afa89ae53e4410ff
camila_fiaminghi <camila.fiaminghi@gmail.com>
2019-01-04 11:31:59 -0200

    add package react-throttle debounce for vote post

--- frontend/src/containers/Post.js
+++ frontend/src/containers/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
+import { Debounce } from 'react-throttle';
 import { handlePosts, handleVotePost, handleRemovePost } from '../actions/posts'
 import { timeDiff, formatTimeDiff } from '../utils/dateUtils'
 
@@ -20,17 +21,21 @@ export class Post extends Component {
 		return (
 			<div className="item">
 				<div className="vote-score">
-					<button
-						onClick={() => handleVote(id, 'upVote')}
-						aria-label="Increase Vote Score">
-						<i className="material-icons">expand_less</i>
-					</button>
+					<Debounce time="100" handler="onClick">
+						<button
+							onClick={() => handleVote(id, 'upVote')}
+							aria-label="Increase Vote Score">
+							<i className="material-icons">expand_less</i>
+						</button>
+					</Debounce>
 					<span>{ voteScore }</span>
-					<button
-						onClick={() => handleVote(id, 'downVote')}
-						aria-label="Decrease Vote Score">
-						<i className="material-icons">expand_more</i>
-					</button>
+					<Debounce time="100" handler="onClick">
+						<button
+							onClick={() => handleVote(id, 'downVote')}
+							aria-label="Decrease Vote Score">
+							<i className="material-icons">expand_more</i>
+						</button>
+					</Debounce>
 				</div>
 				<div className="details">
 					<span className="author"> Posted by <strong>{ author }</strong> { formatTimeDiff(timeDiff(timestamp)) }</span>
